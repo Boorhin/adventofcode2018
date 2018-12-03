d2b9cb5e94ee842213dbbad684a29f3d3a25ec97
Julien Moreau <moreau.juli1@gmail.com>
2018-12-03 19:52:18 +0000

    Update Day2_1.py

--- Day2_1.py
+++ Day2_1.py
@@ -1,10 +1,12 @@
 import numpy as np
 lett = np.genfromtxt('Puzzle2', dtype=np.str)
-n2, n3 = 0,0
+n2, n3, m, n = 0,0, 2,3
 for i in range(len(lett)):
     a, c = np.unique(np.array(list(lett[i])), return_counts=True)
-    n2 += np.count_nonzero(c==2)
-    n3 += np.count_nonzero(c==3)
+    if np.any(c==m):
+        n2 +=1
+    if np.any(c==n):
+        n3 += 1
 print('the number of doublon is ', n2)
 print('the number of triplets is ', n3)
 print('the checksum is ', n2*n3)
