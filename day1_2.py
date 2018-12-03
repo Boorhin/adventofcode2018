047a0903481133dae992c90abe66885b8091fb55
Julien Moreau <moreau.juli1@gmail.com>
2018-12-03 19:14:03 +0000

    Update day1_2.py

--- day1_2.py
+++ day1_2.py
@@ -2,8 +2,10 @@ import numpy as np
 data = np.genfromtxt('Puzzle1')
 looper = True
 l = np.cumsum(data)
+n=0
 while looper:
      a = l[-1]
+     n += 1
      for j in range (len(l)):
          if l[j] in l[:j]:
              print 'found it! it is ',l[j], '\nDepth = ', n
