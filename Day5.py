d459cc7b5e248521d3208fc7a0c3754c085595d9
Julien Moreau <moreau.juli1@gmail.com>
2018-12-06 12:51:51 +0000

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -1,12 +1,22 @@
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+
 with open('Puzzle5') as f:
-  data = f.read()
-L = len(data)
-Continue = True
-while Continue:
-  for i in range(len(data)-1):
-      if data[i] == data[i+1].upper() or data[i].upper() == data[i+1]:
-          data = data[:i]+data[i+2:]
-  if len(data) != L:
-      L = len(data)
-  else:
-      Continue = False  
+	data = f.read().strip('\n')
+L = [len(data)]
+i = 1
+I = [i]
+while i < len(data):
+		host = len(data)
+		if data[i-1] != data[i].swapcase():
+			i += 1
+		else:
+			data = data[:i-1]+data[i+1:]
+			i -= 1
+			L.append(len(data))
+			I.append(i)
+print 'the length of the polymer is ', len(data)
+ax.plot(L,I)
+ax.set_xlabel('Polymer length')
+ax.set_ylabel('verified units')
+plt.show()
