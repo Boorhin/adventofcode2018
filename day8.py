cc80a1ff69dfbc16c128a44261572b9336965a8c
Boorhin <moreau.juli1@gmail.com>
2018-12-09 11:54:35 +0000

    commented not working

--- day8.py
+++ day8.py
@@ -3,29 +3,29 @@ with open('Puzzle8') as f:
     data = f.read().strip().split(' ')
 data = map(int, data)
 metadata = []
-levels = []
-branch = np.zeros(len(data))
+levels = [] #contains the thickness of each branch dynamically
+branch = np.zeros(len(data)) # contains the levels of branching
 todo = []
 i = 0
 met = 0
 while i < len(data)-data[1]:
-    if len(todo) >0 :
-        todo[-1] -= 1 #print i, todo
-    if data[i] > 0:
+    if data[i]+data[i+1] == 0: #empty node
+        i +=2
+    if len(todo) >0: #after each node there is one less to do
+        todo[-1] -= 1
+    if data[i] > 0: #if node branching out
         branch[i+2:] +=1
         levels.append(data[i])
-        todo.append(data[i+1])
+        todo.append(data[i+1]) # the metadata to append when all higher branch terminated
         i +=2
-        #print levels, todo
-    else :
+
+    else : #The local tip of the branch (header 0)
         met = data[i+1]
         for d in data[i+2:i+2+met]:
             metadata.append(d)
-        levels[-1]-= 1
+        levels[-1]-= 1 # one thichness of the tip of the branch thinner
         i += 2+met
-        #print levels, todo
-        if levels[-1] == 0:
-            #print 'here'
+        if levels[-1] == 0: #end of the branch
             branch[i:] -= 1
             for d in data[i: i+1+todo[-1]]:
                 metadata.append(d)
